408adef3c3213e32efead04bd01eb9fe3823aa36
Kishowloy Datta <49715350+Kishowloy117@users.noreply.github.com>
2021-04-06 03:58:52 +0600

    Update imageview.php

--- imageview.php
+++ imageview.php
@@ -8,9 +8,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<h1>
-            Show Image
+<h1 style="text-align: center;">
+            Draft Images
         </h1>
+        
         
 <!-- 
     <div class="container source_next_box">
